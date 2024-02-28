475d6b1eebf157f522add42ffd3fe3bbab735ddd
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-28 23:43:44 +0530

    version 1.0
    set structure

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
