bb1bbe221774a3ad6a50fbda063608ac500b326d
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-29 21:27:58 +0530

    version 1.1

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,25 @@ const cors = require('cors');
 
 const app = express();
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+//Connect to DB and start server
 const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
