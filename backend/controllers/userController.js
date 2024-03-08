0ebe6a025e7292ef8618b81a53bae15ee97c5611
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-03-08 21:07:04 +0530

    version 1.7 add JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,45 +1,61 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  });
+};
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
-    //validation
-    if (!name || !email || !password) {
-        res.status(400)
-        throw new Error('Please fill all the fields')
-    }
-    if (password.length < 6) {
-        res.status(400)
-        throw new Error('Password must be at least 6 characters long')
-    }
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill all the fields');
+  }
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error('Password must be at least 6 characters long');
+  }
 
-    //check if user already exists
-    const userExists = await User.findOne({ email })
+  //check if user already exists
+  const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400)
-        throw new Error('User already exists')
-    }
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
 
-    //create new user
-    const user = await User.create({
-        name,
-        email,
-        password,
-    })
+  //create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    if (user) {
-        const { _id, name, email, photo, phone, bio } = user
-        res.status(201).json({
-            _id, name, email, photo, phone, bio
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid user data')
-    }
-})
+  // Generate token
+  const token = generateToken(user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 module.exports = {
-    registerUser,
-}
+  registerUser,
+};
