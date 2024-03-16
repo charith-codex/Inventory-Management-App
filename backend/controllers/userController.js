942aa1d9c50977d92af987c7d9aaccf46e21ff41
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-03-16 19:08:59 +0530

    version 1.14
    create update user control function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -166,10 +166,38 @@ const loginStatus = asyncHandler(async (req, res) => {
   return res.json(false);
 });
 
+// Update User
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, photo, phone, bio } = user;
+    user.email = email;
+    user.name = req.body.name || name;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+    user.photo = req.body.photo || photo;
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 module.exports = {
   registerUser,
   loginUser,
   logout,
   getUser,
   loginStatus,
+  updateUser,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,12 +6,15 @@ const {
   logout,
   getUser,
   loginStatus,
+  updateUser,
 } = require('../controllers/userController');
 const protect = -require('../middleWare/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/logout', logout);
+router.get('/getuser', protect, getUser);
 router.get('/loggedin', loginStatus);
+router.patch('/updateuser', protect, updateUser);
 
 module.exports = router;
